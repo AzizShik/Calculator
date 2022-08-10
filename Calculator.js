3c5dcf53a0c932452f62560eff4aa7525ad9768b
Abdyl-Aziz <azizshihabov@mail.ru>
2022-08-10 22:09:39 +0300

    feat: finish calculator project

---
+++ Calculator.js
@@ -0,0 +1,81 @@
+export default class Calculator {
+  constructor(primaryOperand, secondaryOperand, operation, notification) {
+    this.primaryOperand = primaryOperand;
+    this.secondaryOperand = secondaryOperand;
+    this.operation = operation;
+    this.notification = notification;
+
+    this.clear();
+  }
+
+  clear() {
+    this.primaryOperand.textContent = 0;
+    this.operation.textContent = '';
+    this.secondaryOperand.textContent = '';
+  }
+
+  addDigit(value) {
+    if (this.primaryOperand.textContent.length >= 11) {
+      this.limit();
+      return;
+    }
+    if (this.primaryOperand.textContent[0] === '0') {
+      this.primaryOperand.textContent = value;
+      return;
+    }
+    this.primaryOperand.textContent += value;
+  }
+
+  removeDigit() {
+    const value = this.primaryOperand.textContent;
+    if (value.length <= 1) {
+      this.primaryOperand.textContent = 0;
+      return;
+    }
+    this.primaryOperand.textContent = value.substring(0, value.length - 1);
+  }
+
+  chooseOperation(operation) {
+    const lastOperation = this.operation.textContent;
+
+    if (lastOperation) {
+      this.operation.textContent = operation;
+      return;
+    }
+
+    this.secondaryOperand.textContent = this.primaryOperand.textContent;
+    this.operation.textContent = operation;
+    this.primaryOperand.textContent = 0;
+  }
+
+  evaluate() {
+    console.log(this.operation.textContent);
+    const value1 = parseFloat(this.secondaryOperand.textContent);
+    const value2 = parseFloat(this.primaryOperand.textContent);
+    this.primaryOperand.textContent = OPERATIONS[this.operation.textContent](
+      value1,
+      value2
+    );
+    if (this.primaryOperand.textContent.length >= 11) {
+      this.limit();
+      this.primaryOperand.textContent = value2;
+      return;
+    }
+    this.secondaryOperand.textContent = '';
+    this.operation.textContent = '';
+  }
+
+  limit() {
+    this.notification.classList.add('active');
+    setTimeout(() => {
+      this.notification.classList.remove('active');
+    }, 1500);
+  }
+}
+
+const OPERATIONS = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+};
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-window.addEventListener('load', () => {});
+import Calculator from './Calculator.js';
+
+const calculatorEl = document.querySelector('.calculator');
+const primaryOperand = document.querySelector('.calculator__primary-operand');
+const secondaryOperand = document.querySelector(
+  '.calculator__secondary-operand'
+);
+const operation = document.querySelector('.calculator__top-operation');
+  const notification = document.querySelector('.notification');
+
+const calculator = new Calculator(primaryOperand, secondaryOperand, operation, notification);
+
+calculatorEl.addEventListener('click', (e) => {
+  if (e.target.matches('[data-all-clear]')) {
+    calculator.clear();
+  }
+  if (e.target.matches('[data-number]')) {
+    calculator.addDigit(e.target.textContent);
+  }
+  if (e.target.matches('[data-delete]')) {
+    calculator.removeDigit();
+  }
+  if (e.target.matches('[data-operation]')) {
+    calculator.chooseOperation(e.target.textContent);
+  }
+  if (e.target.matches('[data-equals]')) {
+    calculator.evaluate();
+  }
+});
